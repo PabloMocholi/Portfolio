57cadf0f8c943cf83a5f08f13b4010bf3f9b3bab
PabloMocholi <pablomocholi66@alumnos.cei.es>
2023-12-30 12:18:08 +0100

    Filtrado

---
+++ js/home.js
@@ -0,0 +1,193 @@
+
+const fallaSection = document.getElementById("FallaSection");
+const ceiSection = document.getElementById("CeiSection");
+const menu = document.getElementById("menu");
+const header = document.getElementById("header");
+const main = document.getElementById("main");
+const filtrado = document.getElementById("filtrado");
+const filtros = document.getElementById("filtros");
+
+
+
+let proyectosFalla = [];
+let proyectosCEI = [];
+let etiquetas = [];
+let vectorActivos = [];
+
+var url = "./db/proyectos.json";
+
+// Uso de la función fetch
+fetch(url)
+    .then(function (response) {
+        if (!response.ok) {
+            throw new Error("La solicitud no fue exitosa: " + response.status);
+        }
+        return response.json();
+    })
+    .then(function (data) {
+        proyectosFalla = data.filter((proyecto) => proyecto.categoria == "Falla");
+        proyectosCEI = data.filter((proyecto) => proyecto.categoria == "CEI");
+        mostrarProyecto();
+        etiquetas = getEtiquetasTotales(data);
+        mostrarEtiquetas(etiquetas);
+
+    })
+
+
+function mostrarProyecto() {
+
+    proyectosFalla.forEach((p) => {
+
+        let tags = "";
+
+        p.etiquetas.forEach((e) => {
+
+            tags += `<span class="ChipsFiltro-chip ${e}">${e}</span>`
+
+        })
+
+        nuevoHTML(fallaSection,p,tags);
+
+    })
+
+    proyectosCEI.forEach((p) => {
+
+        let tags = "";
+
+        p.etiquetas.forEach((e) => {
+
+            tags += `<span class="ChipsFiltro-chip ${e}">${e}</span>`
+
+        })
+
+        nuevoHTML(ceiSection,p,tags);
+    })
+
+}
+
+function menuMostrar() {
+    menu.classList.toggle("active");
+    main.classList.toggle("u-blur");
+}
+
+function filtroMostrar(){
+    filtros.classList.toggle("active");
+    main.classList.toggle("u-blur");
+}
+
+function getEtiquetasTotales(json) {
+
+    let categoriasTotales = [];
+    let encontrado = false;
+
+    json.forEach((elemento) => {
+
+        elemento.etiquetas.forEach((c) => {
+
+            if (categoriasTotales.length == 0) {
+                categoriasTotales.push(c);
+            }
+            else {
+                for (i = 0; i < categoriasTotales.length; i++) {
+                    if (c == categoriasTotales[i])
+                        encontrado = true;
+                }
+
+                if (!encontrado)
+                    categoriasTotales.push(c);
+            }
+
+            encontrado = false;
+
+        })
+
+    });
+
+    return categoriasTotales;
+
+}
+
+function mostrarEtiquetas(etiquetas) {
+
+    etiquetas.forEach((e) => {
+
+        filtrado.innerHTML += `<span class="Filtro" id="${e}" onclick="activarBoton('${e}')"> ${e}</span>`;
+
+    })
+}
+
+function activarBoton(e) {
+
+    let boton = document.getElementById(`${e}`);
+    boton.classList.toggle("button_active");
+
+}
+
+function filtradoTags() {
+
+    fallaSection.innerHTML = `<h3 class="ContenedorProyectos-apartado">Falla Immaterial 2022/2023</h3>`;
+    ceiSection.innerHTML = `<h3 class="ContenedorProyectos-apartado">Proyectos Máster</h3>`;
+  
+    vectorActivos = [];
+
+    let activos = document.querySelectorAll(".button_active");
+    activos.forEach((a) => {
+        vectorActivos.push(a.id);
+    })
+
+    console.log(vectorActivos);
+
+    if(vectorActivos.length>0){
+        comparativa(proyectosFalla,fallaSection);
+        comparativa(proyectosCEI,ceiSection);
+    }else{
+        mostrarProyecto(); 
+    }
+}
+
+function comparativa(proyectos,seccion){
+
+    let encontrado = false;
+
+    proyectos.forEach((p) => {
+
+        encontrado = false;
+        let tags = "";
+
+        p.etiquetas.forEach((e) => {
+
+            tags += `<span class="ChipsFiltro-chip ${e}">${e}</span>`
+            
+            for (i = 0; i < vectorActivos.length; i++) {
+                if (e == vectorActivos[i])
+                    encontrado = true;
+            }
+
+        })
+
+        if(encontrado)
+            nuevoHTML(seccion,p,tags);
+    })
+
+}
+
+
+function nuevoHTML(seccion,p,tags) {
+
+    seccion.innerHTML += `<article>
+    <div class="ContenedorProyectos-proyecto">
+    
+    <div class="ContenedorProyectos-proyecto-fondo">
+    </div>
+    <h3 class="ContenedorProyectos-proyecto-titulo" >
+        ${p.nombre}</h3>
+    
+    <img class="ContenedorProyectos-proyecto-imagen" src=" ${p.imagen}" alt="${p.nombre}">
+    </div>
+
+    <div class="ChipsFiltro">
+    ${tags}
+    </div>
+    </article>`;
+
+}
