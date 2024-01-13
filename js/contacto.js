dbcba308dab5965f82ad892d09445dfb5314d921
PabloMocholi <pablomocholi66@alumnos.cei.es>
2024-01-13 13:32:03 +0100

    Avanzado contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,7 @@
 const menu = document.getElementById("menu");
+const datosEstudios = document.getElementById("datosEstudios")
+const datosLaboral = document.getElementById("datosLaboral")
+const datosCompetencia = document.getElementById("datosCompetecia");
 
 function menuMostrar2() {
     menu.classList.toggle("active");
@@ -12,4 +15,17 @@ function menuMostrar2() {
 
     }
 
+}
+
+
+function cerrar(seccion){
+
+    if(seccion == "estudios"){
+        datosEstudios.classList.toggle("activeE");
+    }else if(seccion == "trabajo"){
+        datosLaboral.classList.toggle("activeL");
+    }
+    else if(seccion == "competencia"){
+        datosCompetencia.classList.toggle("activeC");
+    }
 }
